72714a894915f5ea1026bea5377bfccbad8f9cfe
Keishi Kiuchi <keishikiuchi@Keishi-no-MacBook-Air.local>
2020-01-19 23:31:49 +0900

    [Finish]4-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,7 +26,7 @@ class BooksController < ApplicationController
   def update
   	book = Book.find(params[:id])
   	book.update(book_params)
-  	redirect_to book_path(book.id), :notice => 'Book was successfully created.'
+  	redirect_to book_path(book.id), :notice => 'Book was successfully updated.'
 
   end
 
